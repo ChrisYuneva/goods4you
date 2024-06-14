5da7f42b0c70b46203bb515f13e324b96a132b0c
ChrisYuneva <christina_yuneva@mail.ru>
2024-06-14 15:03:00 +0400

    feat: added storybook

---
+++ src/stories/SearchForm.stories.tsx
@@ -0,0 +1,29 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import SearchForm from '../components/searchForm/searchForm.tsx';
+import {store} from '../app/store/store.ts';
+import {Provider} from 'react-redux';
+
+const meta = {
+    title: 'SearchForm',
+    component: SearchForm,
+    decorators: [
+        Story => (
+            <Provider store={store}>
+                <Story />
+            </Provider>
+        )
+    ],
+    parameters: {
+        layout: 'centered',
+    },
+    tags: ['autodocs'],
+} satisfies Meta<typeof SearchForm>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const DefaultSearchForm: Story = {
+    args: {
+        loading: false
+    }
+};
