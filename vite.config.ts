5df422af3837e9e3a37e77abbbf6dbfbfb093be5
ChrisYuneva <christina_yuneva@mail.ru>
2024-07-10 12:43:19 +0400

    feat: added base in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
+    base: '/goods4you/',
     plugins: [react()],
     css: {
         postcss: {
