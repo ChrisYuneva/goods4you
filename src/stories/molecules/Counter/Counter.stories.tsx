0876b5201732f1fdfc03406220afe3e3e79ac08f
ChrisYuneva <christina_yuneva@mail.ru>
2024-07-10 12:55:46 +0400

    fix: fixed counter stories

--- src/stories/molecules/Counter/Counter.stories.tsx
+++ src/stories/molecules/Counter/Counter.stories.tsx
@@ -17,25 +17,55 @@ type Story = StoryObj<typeof meta>;
 
 export const DefaultCounter: Story = {
     args: {
-        id: 1,
+        product: {
+            id: 1,
+            title: 'Title 1',
+            price: 100,
+            quantity: 4,
+            total: 35,
+            discountPercentage: 7,
+            discountedTotal: 80,
+            thumbnail: ''
+        },
         size: 'default',
         quantity: 1,
+        stock: 3,
     }
 };
 
 export const BigCounter: Story = {
     args: {
-        id: 2,
+        product: {
+            id: 2,
+            title: 'Title 2',
+            price: 200,
+            quantity: 3,
+            total: 35,
+            discountPercentage: 7,
+            discountedTotal: 80,
+            thumbnail: ''
+        },
         size: 'big',
         quantity: 1,
+        stock: 50,
     }
 };
 
 export const CustomClassName: Story = {
     args: {
-        id: 3,
+        product: {
+            id: 3,
+            title: 'Title 3',
+            price: 300,
+            quantity: 7,
+            total: 35,
+            discountPercentage: 7,
+            discountedTotal: 80,
+            thumbnail: ''
+        },
         size: 'default',
         quantity: 1,
+        stock: 58,
         className: styles.customClass,
     }
 };
